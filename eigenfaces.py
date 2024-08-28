1b033d55483de24cc69c77e9997503c1028634be
Lojlk <jkong126822@gmail.com>
2024-08-28 17:48:06 +1000

    updated code for tensor graph

--- eigenfaces.py
+++ eigenfaces.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 import numpy as np
 import sklearn
 import torch
@@ -63,10 +65,11 @@ print(X_test_transformed.shape)
 
 import matplotlib.pyplot as plt
 # Qualitative evaluation of the predictions using matplotlib
+# Qualitative evaluation of the predictions using matplotlib
 def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
     """Helper function to plot a gallery of portraits"""
     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+    plt.subplots_adjust(bottom=0, left=0.01, right=0.99, top=0.90, hspace=0.35)
     for i in range(n_row * n_col):
         plt.subplot(n_row, n_col, i + 1)
         plt.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
@@ -74,32 +77,35 @@ def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
         plt.xticks(())
         plt.yticks(())
 
+
 eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
+
 plt.show()
 
-explained_variance = (S ** 2) / (n_samples - 1)
+# Compactness plot to evaluate performance of the dimensionality reduction
+explained_variance = (S**2) / (n_samples - 1)
 total_var = explained_variance.sum()
 explained_variance_ratio = explained_variance / total_var
-ratio_cumsum = np.cumsum(explained_variance_ratio)
+ratio_cumsum = torch.cumsum(explained_variance_ratio, dim=0)
 print(ratio_cumsum.shape)
-eigenvalueCount = np.arange(n_components)
+eigenvalueCount = torch.arange(n_components)
 plt.plot(eigenvalueCount, ratio_cumsum[:n_components])
-plt.title('Compactness')
+plt.title("Compactness")
 plt.show()
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-#build random forest
+# build random forest
 estimator = RandomForestClassifier(n_estimators=150, max_depth=15, max_features=150)
-estimator.fit(X_transformed, y_train) #expects X as [n_samples, n_features]
+estimator.fit(X_transformed, y_train)  # expects X as [n_samples, n_features]
 predictions = estimator.predict(X_test_transformed)
-correct = predictions==y_test
+predictions = torch.tensor(predictions)
+correct = predictions == y_test
 total_test = len(X_test_transformed)
-#print("Gnd Truth:", y_test)
+# print("Gnd Truth:", y_test)
 print("Total Testing", total_test)
 print("Predictions", predictions)
-print("Which Correct:",correct)
-print("Total Correct:",np.sum(correct))
-print("Accuracy:",np.sum(correct)/total_test)
+print("Which Correct:", correct)
+print("Total Correct:", torch.sum(correct))
+print("Accuracy:", torch.sum(correct) / total_test)
+
 print(classification_report(y_test, predictions, target_names=target_names))

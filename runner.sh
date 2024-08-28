564a23176ac30e16134db774ffd28c82dd0c27dd
Lojlk <jkong126822@gmail.com>
2024-08-28 17:03:18 +1000

    added slurm script

---
+++ runner.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:a100
+#SBATCH --job-name=torch_test
+#SBATCH -o torch_test.out
+conda activate torch
+python eigenfaces.py

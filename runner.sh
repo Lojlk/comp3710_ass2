093c0d1389bc81ecac357bcd0dd4c14485b516ae
Lojlk <jkong126822@gmail.com>
2024-09-03 14:23:42 +1000

    updated runner.sh

--- runner.sh
+++ runner.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:a100
-#SBATCH --job-name=torch_test
-#SBATCH -o torch_test.out
+#SBATCH --job-name=torch_test_dawnbench
+#SBATCH -o torch_test_dawnbench.out
 conda activate torch
-python eigenfaces.py
+python CIFAR10.py

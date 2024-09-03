8cb68ee999e9fda06b9b283c9d8cee47c851c211
Lojlk <jkong126822@gmail.com>
2024-09-03 14:30:04 +1000

    included partition=a100

--- runner.sh
+++ runner.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#SBATCH --partition=a100
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:a100
-#SBATCH --job-name=torch_test_dawnbench
-#SBATCH -o torch_test_dawnbench.out
+#SBATCH --job-name=torch_dawnbench
+#SBATCH -o torch_dawnbench.out
 conda activate torch
 python CIFAR10.py

f1a95d81aa8b25b99e6076be6877cc9e2ccdb084
Lojlk <jkong126822@gmail.com>
2024-09-10 16:31:09 +1000

    runner file for CIFAR

---
+++ runner_CIFAR.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --partition=a100
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:a100
+#SBATCH --job-name=runCIFAR
+#SBATCH -o Result_CIFAR.out
+conda activate demo
+python runGAN.py
